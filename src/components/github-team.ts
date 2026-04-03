a7569ce3bc5d8c06882c87f380da9b28d6d0d75c
john <johnstephensoriao19@gmail.com>
2026-04-03 15:31:41 +0800

    implement the optional GitHub Team args

--- src/components/github-team.ts
+++ src/components/github-team.ts
@@ -4,6 +4,13 @@ import * as github from "@pulumi/github";
 export interface GitHubTeamComponentArgs {
   teamSlug: string;
   description?: string;
+  privacy?: "closed" | "secret";
+  parentTeamId?: string;
+  parentTeamReadId?: string;
+  parentTeamReadSlug?: string;
+  notificationSetting?: string;
+  ldapDn?: string;
+  createDefaultMaintainer?: boolean;
 }
 
 export class GitHubTeamComponent extends pulumi.ComponentResource {
@@ -21,6 +28,13 @@ export class GitHubTeamComponent extends pulumi.ComponentResource {
       {
         name: args.teamSlug,
         description: args.description,
+        ...(args.privacy !== undefined && { privacy: args.privacy }),
+        ...(args.parentTeamId !== undefined && { parentTeamId: args.parentTeamId }),
+        ...(args.parentTeamReadId !== undefined && { parentTeamReadId: args.parentTeamReadId }),
+        ...(args.parentTeamReadSlug !== undefined && { parentTeamReadSlug: args.parentTeamReadSlug }),
+        ...(args.notificationSetting !== undefined && { notificationSetting: args.notificationSetting }),
+        ...(args.ldapDn !== undefined && { ldapDn: args.ldapDn }),
+        ...(args.createDefaultMaintainer !== undefined && { createDefaultMaintainer: args.createDefaultMaintainer }),
       },
       { parent: this },
     );
